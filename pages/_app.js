ddab3af580951a3b05e569cd13fe56e1e4623fa1
ARTAS <artasss47@gmail.com>
2021-04-19 22:46:03 +0200

    refactored _app.js to be function component, trying out getInitialProps

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,24 @@
-import App from 'next/app';
-import Layout from '../components/Layout/Layout';
-import 'semantic-ui-css/semantic.min.css';
+import Layout from "../components/Layout/Layout";
+import "semantic-ui-css/semantic.min.css";
 
-import React from 'react'
+import React from "react";
 
-class MyApp extends App {
-    render() {
-        const {Component} = this.props;
-
-        return (
-            <Layout>
-                <Component />
-            </Layout>   
-        )
-    }
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout {...pageProps}>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
+
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+  let pageProps = {};
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  return { pageProps };
+};
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
-const Index = () => {
-    return (
-        <div>
-            Homepage
-        </div>
-    )
-}
+const Index = ({ posts }) => {
+  return (
+    <div>
+      {posts && posts.length > 0 && posts.map((post) => <h1>{post.title}Z</h1>)}
+    </div>
+  );
+};
 
-export default Index
+Index.getInitialProps = async (ctx) => {
+  try {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    const { name } = ctx.query;
+    return { posts: res.data };
+  } catch (err) {}
+};
+
+export default Index;
