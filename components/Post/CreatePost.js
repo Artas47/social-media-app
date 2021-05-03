bfc9f6935cb62a78d3768172c33b792be8104819
Artas47 <artasss47@gmail.com>
2021-05-03 16:21:47 +0200

    finished CreatePost component

--- components/Post/CreatePost.js
+++ components/Post/CreatePost.js
@@ -27,6 +27,11 @@ const CreatePost = ({user, setPosts}) => {
     const handleSubmit = (e) => {
         e.preventDefault();
     }
+
+    const addStyles = () => ({
+        textAlign: 'center', height: '150px', width: '150px', border: 'dotted', paddingTop: media===null && '60px', cursor: 'pointer', borderColor: highlighted ? 'green' : 'black'
+    });
+
   return <>
   <Form error={error !== null} onSubmit={handleSubmit}>
     <Message error onDismiss={() => setError(null)} content={error} header='Oops!'/>
@@ -34,7 +39,37 @@ const CreatePost = ({user, setPosts}) => {
         <Image src={user.profilePicUrl} circular avatar inline />
         <Form.TextArea placeholder='Whats happening?' name='text' value={newPost.text} onChange={handleChange} rows={4} width={14}/>
     </Form.Group>
+    <Form.Group>
+        <Form.Input value={newPost.location} name='location' onChange={handleChange} label='Add location' icon='map marker alternate' placeholder='Want to add location?' />
+        <input ref={inputRef} onChange={handleChange} name='media' style={{display: 'none'}} type='file' accept='image' />
+    </Form.Group>
+
+    <div 
+    onDrag={e => {
+        e.preventDefault();
+        setHighlighted(true);
+    }}
+    onDragLeave={e => {
+        e.preventDefault();
+        setHighlighted(false);
+    }}
+    onDrop={e => {
+        e.preventDefault();
+        setHighlighted(true);
+        const droppedFile = Array.from(e.dataTransfer.files);
+        setMedia(droppedFile[0]);
+        setMediaPreview(URL.createObjectURL(droppedFile[0]))
+    }}
+    style={addStyles()}>
+        {media === null ?<Icon name='plus' onClick={() => inputRef.current.click()} size='big' />
+        : <> 
+        <Image src={mediaPreview} style={{height: '150px', width: '150px'}} alt='PostImage' centered size='medium' onClick={() => inputRef.current.click()} />
+        </>}
+    </div>
+    <Divider hidden />
+    <Button circular disabled={newPost.text === '' || loading} content={<strong>Post</strong>} loading={loading} icon='send' style={{backgroundColor: '#1DA1F2', color: '#fff'}}/>
   </Form>
+  <Divider />
   </>;
 };
 
