a2da1edd35c8f572a29c20c5aa47a05af106ce8c
Artas47 <artasss47@gmail.com>
2021-06-01 21:29:47 +0200

    routes for getting profile and posts of user

---
+++ api/profile.js
@@ -0,0 +1,45 @@
+const authMiddleware = require("../middleware/authMiddleware");
+const FollowerModel = require("../models/FollowerModel");
+const PostModel = require("../models/PostModel");
+const ProfileModel = require("../models/ProfileModel");
+const UserModel = require("../models/UserModel");
+const router = express.Router();
+
+router.get('/:username', authMiddleware, async(req,res) => {
+    const {username} = req.params;
+    
+    try {
+        const user = await UserModel.findOne({username: username.toLowerCase()});
+        if(!user){
+            return res.status(404).send('User not found')
+        }
+        const profile = await ProfileModel.findOne({user: user._id}).populate('user');
+
+        const profileFollowStats = await FollowerModel.findOne({user: user._id})
+
+        return res.json({
+            profile,
+            followersLength: profileFollowStats.followers.length > 0 ? profileFollowStats.followers.length : 0,
+            followingLength: profileFollowStats.following.length > 0 ? profileFollowStats.following.length : 0
+        })
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Server error')
+    }
+})
+
+router.get('/posts/:username', authMiddleware, async(req,res) => {
+    const {username} = req.params;
+    try {
+        const user = await UserModel.findOne({username: username.toLowerCase()});
+        if(!user){
+            return res.status(404).send('User not found')
+        }
+        const posts = await PostModel.find({user: user._id}).sort({createdAt: -1}).populate('user').populate('comments.user')
+
+        return res.json(posts);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Server error')
+    }
+})
